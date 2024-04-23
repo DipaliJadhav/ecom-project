c7e074df0231b4d598198fcef77d619eb9b473bc
Dipali <jdipali2810@gmail.com>
2024-04-23 12:30:03 +0530

    New Pages Created At 23-04-2024

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Header';
+import Login from './Login';
+import AddProduct from './AddProduct';
+import EditProduct from './EditProduct';
+import Register from './Register';
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Header />
       <h1>Ecom Project</h1>
+      <Routes>
+      <Route path="/login" element={<Login />} /> 
+      <Route path="/register" element={<Register />} /> 
+      <Route path="/add" element={<AddProduct />} /> 
+      <Route path="/edit" element={<EditProduct />} /> 
+     
       
-      <Button>Bootstarp Button</Button>
-      <button>Noraml Button</button>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+function Login() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+        <p>dummy text dummy text dummy text dummy text dummy text 
+            dummy text dummy text dummy text dummy text dummy text
+             dummy text dummy text dummy text dummy text dummy text dummy
+              text dummy text dummy text dummy text dummy text dummy text
+               dummy text dummy text dummy text dummy text dummy text dummy text dummy text </p>
+      </div>
+    );
+  }
+  
+  export default Login;
